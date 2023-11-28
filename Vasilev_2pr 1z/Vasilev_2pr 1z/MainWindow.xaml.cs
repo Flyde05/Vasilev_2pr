116b43e32e42c06d94dfc21d12e51b16849b5044
Flyde05 <151150338+Flyde05@users.noreply.github.com>
2023-11-28 13:36:56 +0300

    Add files via upload

---
+++ Vasilev_2pr 1z/Vasilev_2pr 1z/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vasilev_2pr_1z
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           if(!double.TryParse(a.Text ,out double b))
+            {
+                MessageBox.Show("Поле байт только числа");
+            }
+            double kb = Math.Floor(b / 1024);
+            MessageBox.Show($"kb={kb,1:f0}");
+        }
+    }
+}
